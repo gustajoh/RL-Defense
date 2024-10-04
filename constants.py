ccb930e83d9c9faba626705f67a6a75e71181550
Gustaf Johansson <gustajoh@kth.se>
2024-10-04 15:42:43 +0200

    added actions

--- constants.py
+++ constants.py
@@ -4,13 +4,28 @@ VM_PASSWORD = 'gns3'
 PROJECT_ID = '3317c6f6-5a8f-4a89-a09c-b59cae565e0d'
 
 DOCKER_NODES = {
-    "AdminPC": "27b7647b6071" ,
+    "AdminPC": "27b7647b6071",
     "COZYBEAR": "bbfc0e11fd42",
-    "IDPS" : "05ea4b2441c6",
-    "RepoServer" : "5ba20cc85042",
-    "MgmHost" : "2835fe355648",
-    "Fileshare" : "5aaad19d7dca"
+    "IDPS": "05ea4b2441c6",
+    "RepoServer": "5ba20cc85042",
+    "MgmHost": "2835fe355648",
+    "Fileshare": "5aaad19d7dca"
 }
 
+USERS = ['james', 'alica', 'bob']
 # Console commands
 BRUTE_FORCE_ATTACK = 'medusa -h %s -U users.txt -P passwords.txt -M ssh | grep FOUND'
+
+# Todo: update with more appropriate rules for snort and shorewall
+SNORT_RULES = ['alert icmp any any -> $HOME_NET any (msg:"ICMP Ping"; sid:1000001; rev:1;)',
+               'alert tcp any any -> $HOME_NET 80 (msg:"HTTP Traffic"; sid:1000002; rev:1;)',
+               'alert udp any any -> $HOME_NET 53 (msg:"DNS Query"; sid:1000003; rev:1;)']
+
+SHOREWALL_RULES = [
+    "ACCEPT   net      fw     tcp       80",
+    "ACCEPT   net      fw     tcp       443",
+    "DROP     net:192.168.1.100     all",
+    "ACCEPT   lan      fw     tcp       22",
+    "DROP     net:192.168.2.0/24    all",
+    "ACCEPT   net      fw     icmp"
+]
--- network_defense_env.py
+++ network_defense_env.py
@@ -2,6 +2,8 @@ from network_interactions import *  # Import your functions
 import gym
 from gym import spaces
 import numpy as np
+from constants import *
+
 
 class NetworkDefenseEnv(gym.Env):
     def __init__(self):
@@ -10,26 +12,30 @@ class NetworkDefenseEnv(gym.Env):
         commands = 8
         nodes = 5
         users = 3
+        snort_rules = 3
+        firewall_rules = 6
 
-
-
-        self.action_space = spaces.MultiDiscrete([types, commands, nodes, users])  # [Category, Specific Action in that category]
+        # [Category, Specific Action in that category]
+        self.action_space = spaces.MultiDiscrete(
+            [types, commands, nodes, users, snort_rules, firewall_rules])
 
     def step(self, action):
         response_type = action[0]
         specific_action = action[1]
         node = action[3]
         user = action[4]
-        
+        snort_rule = action[5]
+        firewall_rule = action[6]
+
         # Detection
-        if response_type == 0:  
+        if response_type == 0:
             if specific_action == 0:
-                self.add_snort_rule()
+                self.add_snort_rule(self, node, snort_rule)
             elif specific_action == 1:
-                self.add_firewall_rule()
-        
+                self.add_firewall_rule(self, node, firewall_rule)
+
         # Mitigation
-        elif response_type == 1:  
+        elif response_type == 1:
             if specific_action == 0:
                 self.limit_traffic(node)
 
@@ -38,15 +44,15 @@ class NetworkDefenseEnv(gym.Env):
 
             elif specific_action == 2:
                 self.limit_user(node, user)
-        
+
         # Containment
-        elif response_type == 2:  
+        elif response_type == 2:
             if specific_action == 0:
                 self.turn_off_node(node)
-            
+
             elif specific_action == 1:
                 self.isolate_node(node)
-            
+
             elif specific_action == 2:
                 self.migrate_node(node)
 
@@ -55,24 +61,40 @@ class NetworkDefenseEnv(gym.Env):
         done = 'not implemented'
 
         return observation, reward, done, {}
-# Detection   
-    def add_snort_rule(self, node_id):
-        command = 'not implemented'
+# Detection
+
+    # Adds a random snort rule from constants.py and restarts snort to apply rule
+    def add_snort_rule(self, node_id, index):
+        rule = constants.SNORT_RULES[index]
+        command = (
+            f'echo "{rule}" >> /etc/snort/rules/local.rules '
+            '&& snort -c /etc/snort/snort.conf -i eth0 eth1 eth2'
+        )
         execute_command(node_id, command)
-    
-    def add_firewall_rule(self, node_id):
-        command = 'not implemented'
+        print("SNORT rule added")
+
+    # Adds shorewall rule from constants.py, restarts shorewall to apply
+    def add_firewall_rule(self, node_id, index):
+        rule = constants.SHOREWALL_RULES[index]
+        command = (f'echo "{rule}" >> /etc/shorewall/rules '
+                   '&& shorewall restart')
         execute_command(node_id, command)
+        print("Firewall rule added")
 
 # Mitigation
-    def blacklist_ip(self, node_id):
-        command = 'not implemented'
+
+    # Adds target IP to blacklist and restarts firewall to apply updated blacklist
+    def blacklist_ip(self, node_id, target_ip):
+        command = (
+            f'echo {target_ip} >> /etc/shorewall/blacklist '
+            '&& shorewall restart'
+        )
         execute_command(node_id, command)
-    
+
     def limit_user(self, node_id, user):
-        command = 'not implemented, make case for each user'
+        command = f'deluser {constants.USERS[user]} sudo'
         execute_command(node_id, command)
-    
+
     def limit_traffic(self, node_id):
         command = 'not implemented'
         execute_command(node_id, command)
@@ -82,16 +104,23 @@ class NetworkDefenseEnv(gym.Env):
         stop_node(node_id)
         print(f"Node {node_id} turned off")
 
-    def migrate_node(self, node_id):
-        command = 'not implemented'
+    # Configures new IP within the DMZ zone and updates routing
+    def migrate_node(self, node_id, new_ip, gateway_ip):
+        command = (
+            f'ifconfig eth0 {new_ip} netmask 255.255.255.0 '
+            f'&& route add default gw {gateway_ip}'
+        )
         execute_command(node_id, command)
 
+    # Disables inbound and outbound traffic
     def isolate_node(self, node_id):
         command = "ifconfig eth0 down"
         execute_command(node_id, command)
         print(f"Node {node_id} isolated (interface down)")
 
-# other (maybe not usable here)
+
+# Other (maybe not usable here)
+
     def start_node(self, node_id):
         start_node(node_id)
         print(f"Node {node_id} restarted")
@@ -99,5 +128,3 @@ class NetworkDefenseEnv(gym.Env):
     def restart_node(self, node_id):
         restart_node(node_id)
         print(f"Node {node_id} restarted")
-
-
