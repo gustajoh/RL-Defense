aba9071ed85042940da54d8a44d7b354537a74d1
Gustaf Johansson <gustajoh@kth.se>
2024-12-20 14:03:38 +0100

    concatenate err_output

--- network_interactions.py
+++ network_interactions.py
@@ -11,7 +11,6 @@ def execute_command(node_id, command):
         # Connect to the GNS3 VM
         ssh_client.connect(hostname=constants.GNS3_VM,
                            username=constants.VM_USERNAME, password=constants.VM_PASSWORD)
-        print("SSH connection established successfully!")
 
         exec_command = f'docker exec -i {node_id} {command}'
         _, stdout, stderr = ssh_client.exec_command(exec_command)
@@ -20,7 +19,8 @@ def execute_command(node_id, command):
 
         error = stderr.read().decode('utf-8')
         if error:
-            print("Error output:\n", error)
+            output += error
+            #print("Error output:\n", error)
         return output
 
     except paramiko.AuthenticationException:
@@ -32,8 +32,6 @@ def execute_command(node_id, command):
     finally:
         # Close connection
         ssh_client.close()
-        print("SSH connection closed.")
-
 
 def start_node(node):
     URL = ('http://192.168.33.7:3080/v2/projects/'
@@ -54,6 +52,45 @@ def stop_node(node):
     )
     return response
 
+def start_nodes():
+    URL = ('http://192.168.33.7:3080/v2/projects/'
+           f'{constants.PROJECT_ID}/nodes/start')
+    response = requests.post(
+        URL,
+        headers={'Content-Type': 'application/x-www-form-urlencoded', }
+    )
+    return response
+
+def stop_nodes():
+    URL = ('http://192.168.33.7:3080/v2/projects/'
+        f'{constants.PROJECT_ID}/nodes/stop')
+    response = requests.post(
+        URL,
+        headers={'Content-Type': 'application/x-www-form-urlencoded', }
+    )
+    return response
+
+def restart_sim():
+    print("Restarting")
+    URL = 'http://192.168.33.7:3080/v2/projects/31d6b89d-08f6-4eba-8d7d-0ed7a19579b4/snapshots'
+    response = requests.get(URL, headers={})
+    id = ''
+    for snapshot in response.json():
+        if snapshot['name'] == "prelim_snapshot":
+            id = snapshot['snapshot_id']
+
+    if id == '':
+        print("Critical error: Cannot find snapshot")
+        return 
+    
+    URL = ('http://192.168.33.7:3080/v2/projects/'
+        f'{constants.PROJECT_ID}/snapshots/{id}/restore')
+    response = requests.post(
+        URL,
+        headers={'Content-Type': 'application/x-www-form-urlencoded', }
+    )
+    print(response)
+    return response
 
 def restart_node(node):
     start_url = ('http://192.168.33.7:3080/v2/projects/'
