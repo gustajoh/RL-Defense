a449a03cdd25276db91a71359add8248105f7b53
Gustaf Johansson <gustajoh@kth.se>
2025-04-01 12:57:45 +0200

    added check_env

---
+++ check_env.py
@@ -0,0 +1,9 @@
+from constants import *
+from network_attack_env import *
+from network_defense_env import *
+from stable_baselines3.common.env_checker import check_env
+
+
+env = NetworkDefenseEnv()
+
+check_env(env)
