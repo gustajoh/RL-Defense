406cb82239526794543982fcb80dc6d42f33bffd
Gustaf Johansson <gustajoh@kth.se>
2025-04-01 12:55:27 +0200

    separated files for training different models

---
+++ LSTM_RPPO.py
@@ -0,0 +1,31 @@
+from network_defense_env import *
+from stable_baselines3.common.callbacks import CheckpointCallback
+from sb3_contrib import RecurrentPPO
+
+
+env = NetworkDefenseEnv()
+
+model = RecurrentPPO("MultiInputLstmPolicy",
+                     env,
+                     n_steps=20,
+                     batch_size=10,
+                     verbose=1,
+                     policy_kwargs={"lstm_hidden_size": 256}
+                     )
+
+checkpoint_callback = CheckpointCallback(
+    save_freq=500,
+    save_path="./model_checkpoints/",
+    name_prefix="lstm_rppo_checkpoint"
+)
+
+try:
+    model.learn(total_timesteps=5000, progress_bar=True, callback=checkpoint_callback)
+except KeyboardInterrupt:
+    print("Interrupted! Saving..")
+    model.save("Emergency_checkpoint")
+except Exception as e:
+    print(f"Unexpected error: {e} Saving model")
+    model.save("Unknown_checkpoint")
+
+model.save("RPPO_Model")
--- PPO.py
+++ PPO.py
@@ -1,16 +1,12 @@
-from constants import *
-import numpy as np
-from network_attack_env import *
 from network_defense_env import *
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.callbacks import CheckpointCallback
 
 
-def_env = NetworkDefenseEnv()
-#check_env(def_env)
+env = NetworkDefenseEnv()
 
-model = PPO("MultiInputPolicy", def_env, verbose=1, n_steps=50, batch_size=10)
+model = PPO("MultiInputPolicy", env, verbose=1, n_steps=20, batch_size=10)
+
 checkpoint_callback = CheckpointCallback(
     save_freq=500,
     save_path="./model_checkpoints/",
@@ -18,7 +14,7 @@ checkpoint_callback = CheckpointCallback(
 )
 
 try:
-    model.learn(total_timesteps=3000, progress_bar=True, callback=checkpoint_callback)
+    model.learn(total_timesteps=5000, progress_bar=True, callback=checkpoint_callback)
 except KeyboardInterrupt:
     print("Interrupted! Saving..")
     model.save("Emergency_checkpoint")
@@ -26,4 +22,4 @@ except Exception as e:
     print(f"Unexpected error: {e} Saving model")
     model.save("Unknown_checkpoint")
 
-model.save("PPO_300")
+model.save("PPO_v2")
